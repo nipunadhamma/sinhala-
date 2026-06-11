13746e85018e8c4361f294fcb7595857300f9f44
nipunadhamma <kalawanesangamaji@gmail.com>
2026-06-12 00:49:23 +0530

    Create transliterator.js

---
+++ engine/transliterator.js
@@ -0,0 +1,12 @@
+import {VOWELS} from "./vowels.js";
+import {CONSONANTS} from "./consonants.js";
+import {MODIFIERS} from "./modifiers.js";
+
+export function transliterate(text){
+
+    // දැනට පළමු පියවරේදී
+    // input එක 그대로 return කරනවා.
+
+    return text;
+
+}
