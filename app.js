83b007e6283fae1942a11b77670648ca8b0b887b
nipunadhamma <kalawanesangamaji@gmail.com>
2026-06-12 00:56:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-const input=document.getElementById("roman");
-const output=document.getElementById("sinhala");
-
-input.addEventListener("input",()=>{
-
-output.value=input.value;
-
-});
-import {transliterate} from "./engine/transliterator.js";
+import { transliterate } from "./engine/transliterator.js";
 
 const input = document.getElementById("roman");
 const output = document.getElementById("sinhala");
