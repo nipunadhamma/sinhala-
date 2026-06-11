90595d1b38b8f18afe0065d7f3afb205627cf7bb
nipunadhamma <kalawanesangamaji@gmail.com>
2026-06-12 02:38:55 +0530

    Update dictionary.js

--- engine/dictionary.js
+++ engine/dictionary.js
@@ -1,24 +1,13 @@
 export const DICTIONARY = {
 
-    "amma": "අම්මා",
-    "anna": "අන්න",
-    "ganna": "ගන්න",
-    "balanna": "බලන්න",
-
-    "mama": "මම",
-    "oba": "ඔබ",
-    "api": "අපි",
-
-    "kohomada": "කොහොමද",
-    "kohomadawada": "කොහොමද",
-
-    "ehema": "එහෙම",
-    "mehama": "මෙහෙම",
-
-    "sinhala": "සිංහල",
-    "buddha": "බුද්ධ",
-
-    "dan": "දැන්",
-    "thama": "තම"
+"amma":"අම්මා",
+"mage":"මගේ",
+"oba":"ඔබ",
+"api":"අපි",
+"kohomada":"කොහොමද",
+"sri":"ශ්‍රී",
+"buddha":"බුද්ධ",
+"ganna":"ගන්න",
+"balanna":"බලන්න"
 
 };
