4363adbfb7585e663b5a0b35abf4d1be55f052d6
nipunadhamma <kalawanesangamaji@gmail.com>
2026-06-12 02:25:15 +0530

    Create hal.js

---
+++ engine/hal.js
@@ -0,0 +1,31 @@
+export function isConsonant(c){
+
+const consonants = "kgcjtdnpbmyrlwsh";
+
+return consonants.includes(c);
+
+}
+export const HAL = {
+
+"k":"ක්",
+"g":"ග්",
+"c":"ච්",
+"j":"ජ්",
+
+"t":"ට්",
+"d":"ඩ්",
+"n":"න්",
+
+"p":"ප්",
+"b":"බ්",
+
+"m":"ම්",
+"y":"ය්",
+"r":"ර්",
+"l":"ල්",
+"w":"ව්",
+
+"s":"ස්",
+"h":"හ්"
+
+};
